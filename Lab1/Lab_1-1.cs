c735b7bcab99032c7c7b89b579aa5381fcb662a8
LevKolechkov <levkolechkov@gmail.com>
2023-09-29 23:42:31 +0700

    Доработана задача №1 и добавлен лист Excel для первой задачи

--- Lab1/Lab_1-1.cs
+++ Lab1/Lab_1-1.cs
@@ -6,36 +6,40 @@ namespace Lab
   {
     static void Main(string[] args)
     {
-      int L; // длина бруска
-      int a,b; // рёбра бруска
+      double L, a, b, l, r, volumeOfBlock, volumeOfCylinder;
+      double Pi = Math.PI;
 
-      int l; // длина цилиндра
-      int r; // радиус цилиндра
+      try
+      {
+        Console.WriteLine("Введите длину бруска (L)");
+        L = Convert.ToDouble(Console.ReadLine());
 
-      int volumeOfBlock; // объём бруска
-      int volumeOfCylinder; // объём цилиндра
-      
-      /*
-      * Объём бруска volumeOfBlock = L * a * b
-      * Объём цилиндра равен volumeOfCylinder = l * π*r^2
-      * 
-      * Чтобы получить процент материала (perc), ушедшего в отходы необходмо volumeOfBlock / volumeOfCylinder * 100
-      */
+        Console.WriteLine("Введите длину ребра бруска (a)");
+        a = Convert.ToDouble(Console.ReadLine());
 
-      // Пусть 
-      L = 50;
-      a, b = 20;
+        Console.WriteLine("Введите длину ребра бруска (b)");
+        b = Convert.ToDouble(Console.ReadLine());
 
-      l = 30;
-      r = 10;
+        Console.WriteLine("Введите длину цилиндра (l)");
+        l = Convert.ToDouble(Console.ReadLine());
 
-      // Тогда
+        Console.WriteLine("Введите радиус цилиндра (r)");
+        r = Convert.ToDouble(Console.ReadLine());
+      }
+      catch (FormatException)
+      {
+        Console.WriteLine("Неправильно введенные данные. Попробуйте снова.");
+        return;
+      }
+
       volumeOfBlock = L * a * b;
-      volumeOfCylinder = l * Math.Pi * r * r;
+      volumeOfCylinder = l * Pi * r * r;
 
-      perc = volumeOfBlock / volumeOfCylinder * 100;
+      double perc = 100 - (volumeOfCylinder / volumeOfBlock) * 100;
 
-      console.writeline(volumeOfBlock, volumeOfCylinder, perc)
+      Console.WriteLine($"Объем бруска: {volumeOfBlock}");
+      Console.WriteLine($"Объем цилиндра: {volumeOfCylinder}");
+      Console.WriteLine($"Процент материала, ушедшего в отходы: {perc}%");
     }
   }
 }
