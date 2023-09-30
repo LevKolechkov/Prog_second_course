29eb79422999fb393161bdc4f7b38a29c14ed198
Колечков Лев <levkolechkov@gmail.com>
2023-09-30 09:21:38 +0700

    Написаны дополнительные условия ввода данных

--- Lab1/Lab_1-1.cs
+++ Lab1/Lab_1-1.cs
@@ -9,24 +9,44 @@ namespace Lab
       double L, a, b, l, r, volumeOfBlock, volumeOfCylinder;
       double Pi = Math.PI;
 
+      Exception checkForException(string param)
+      {
+        if (Convert.ToDouble(param) <= 0) throw new Exception();
+        if (param.IndexOf(" ") >= 0) throw new Exception();
+        return null;
+        
+      }
+
       try
       {
         Console.WriteLine("Введите длину бруска (L)");
-        L = Convert.ToDouble(Console.ReadLine());
+        string L1 = Console.ReadLine();
+        checkForException(L1);
+        L = Convert.ToDouble(L1);
 
         Console.WriteLine("Введите длину ребра бруска (a)");
-        a = Convert.ToDouble(Console.ReadLine());
+        string a1 = Console.ReadLine();
+        checkForException(a1);
+        a = Convert.ToDouble(a1);
 
         Console.WriteLine("Введите длину ребра бруска (b)");
-        b = Convert.ToDouble(Console.ReadLine());
+        string b1 = Console.ReadLine();
+        checkForException(b1);
+        b = Convert.ToDouble(b1);
 
         Console.WriteLine("Введите длину цилиндра (l)");
-        l = Convert.ToDouble(Console.ReadLine());
+        string l1 = Console.ReadLine();
+        checkForException(l1);
+        l = Convert.ToDouble(l1);
 
         Console.WriteLine("Введите радиус цилиндра (r)");
-        r = Convert.ToDouble(Console.ReadLine());
+        string r1 = Console.ReadLine();
+        checkForException (r1);
+        r = Convert.ToDouble(r1);
+
+        if (r > a / 2 || r > b / 2 || l > L) throw new Exception();
       }
-      catch (FormatException)
+      catch (Exception)
       {
         Console.WriteLine("Неправильно введенные данные. Попробуйте снова.");
         return;
