460ed46dd9a5ebcc5f930dcf32224b199ddfb83e
LevKolechkov <levkolechkov@gmail.com>
2023-12-07 23:33:18 +0700

    Создана заготовка проекта

---
+++ Lab6/Интегралы/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace Интегралы
+{
+  partial class Form1
+  {
+    /// <summary>
+    /// Обязательная переменная конструктора.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Освободить все используемые ресурсы.
+    /// </summary>
+    /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Код, автоматически созданный конструктором форм Windows
+
+    /// <summary>
+    /// Требуемый метод для поддержки конструктора — не изменяйте 
+    /// содержимое этого метода с помощью редактора кода.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+      System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+      System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+      this.textBox1 = new System.Windows.Forms.TextBox();
+      this.label1 = new System.Windows.Forms.Label();
+      this.label2 = new System.Windows.Forms.Label();
+      this.textBox2 = new System.Windows.Forms.TextBox();
+      this.label3 = new System.Windows.Forms.Label();
+      this.textBox3 = new System.Windows.Forms.TextBox();
+      this.label4 = new System.Windows.Forms.Label();
+      this.textBox4 = new System.Windows.Forms.TextBox();
+      this.chartOfIntegrals = new System.Windows.Forms.DataVisualization.Charting.Chart();
+      this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+      this.рассчитатьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+      this.очиститьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+      ((System.ComponentModel.ISupportInitialize)(this.chartOfIntegrals)).BeginInit();
+      this.menuStrip1.SuspendLayout();
+      this.SuspendLayout();
+      // 
+      // textBox1
+      // 
+      this.textBox1.Location = new System.Drawing.Point(31, 70);
+      this.textBox1.Name = "textBox1";
+      this.textBox1.Size = new System.Drawing.Size(166, 20);
+      this.textBox1.TabIndex = 0;
+      // 
+      // label1
+      // 
+      this.label1.AutoSize = true;
+      this.label1.Location = new System.Drawing.Point(31, 51);
+      this.label1.Name = "label1";
+      this.label1.Size = new System.Drawing.Size(97, 13);
+      this.label1.TabIndex = 1;
+      this.label1.Text = "Введите функцию";
+      // 
+      // label2
+      // 
+      this.label2.AutoSize = true;
+      this.label2.Location = new System.Drawing.Point(31, 126);
+      this.label2.Name = "label2";
+      this.label2.Size = new System.Drawing.Size(66, 13);
+      this.label2.TabIndex = 3;
+      this.label2.Text = "Интервал А\r\n";
+      // 
+      // textBox2
+      // 
+      this.textBox2.Location = new System.Drawing.Point(31, 142);
+      this.textBox2.Name = "textBox2";
+      this.textBox2.Size = new System.Drawing.Size(166, 20);
+      this.textBox2.TabIndex = 2;
+      // 
+      // label3
+      // 
+      this.label3.AutoSize = true;
+      this.label3.Location = new System.Drawing.Point(31, 201);
+      this.label3.Name = "label3";
+      this.label3.Size = new System.Drawing.Size(66, 13);
+      this.label3.TabIndex = 5;
+      this.label3.Text = "Интервал Б";
+      // 
+      // textBox3
+      // 
+      this.textBox3.Location = new System.Drawing.Point(31, 217);
+      this.textBox3.Name = "textBox3";
+      this.textBox3.Size = new System.Drawing.Size(166, 20);
+      this.textBox3.TabIndex = 4;
+      // 
+      // label4
+      // 
+      this.label4.AutoSize = true;
+      this.label4.Location = new System.Drawing.Point(31, 272);
+      this.label4.Name = "label4";
+      this.label4.Size = new System.Drawing.Size(54, 13);
+      this.label4.TabIndex = 7;
+      this.label4.Text = "Точность";
+      // 
+      // textBox4
+      // 
+      this.textBox4.Location = new System.Drawing.Point(31, 288);
+      this.textBox4.Name = "textBox4";
+      this.textBox4.Size = new System.Drawing.Size(166, 20);
+      this.textBox4.TabIndex = 6;
+      // 
+      // chartOfIntegrals
+      // 
+      chartArea2.Name = "ChartArea1";
+      this.chartOfIntegrals.ChartAreas.Add(chartArea2);
+      legend2.Enabled = false;
+      legend2.Name = "Legend1";
+      this.chartOfIntegrals.Legends.Add(legend2);
+      this.chartOfIntegrals.Location = new System.Drawing.Point(303, 51);
+      this.chartOfIntegrals.Name = "chartOfIntegrals";
+      series2.BorderWidth = 3;
+      series2.ChartArea = "ChartArea1";
+      series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+      series2.Legend = "Legend1";
+      series2.Name = "Series1";
+      this.chartOfIntegrals.Series.Add(series2);
+      this.chartOfIntegrals.Size = new System.Drawing.Size(401, 301);
+      this.chartOfIntegrals.TabIndex = 8;
+      this.chartOfIntegrals.Text = "chart1";
+      // 
+      // menuStrip1
+      // 
+      this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.рассчитатьToolStripMenuItem,
+            this.очиститьToolStripMenuItem});
+      this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+      this.menuStrip1.Name = "menuStrip1";
+      this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+      this.menuStrip1.TabIndex = 9;
+      this.menuStrip1.Text = "menuStrip1";
+      // 
+      // рассчитатьToolStripMenuItem
+      // 
+      this.рассчитатьToolStripMenuItem.Name = "рассчитатьToolStripMenuItem";
+      this.рассчитатьToolStripMenuItem.Size = new System.Drawing.Size(80, 20);
+      this.рассчитатьToolStripMenuItem.Text = "Рассчитать";
+      // 
+      // очиститьToolStripMenuItem
+      // 
+      this.очиститьToolStripMenuItem.Name = "очиститьToolStripMenuItem";
+      this.очиститьToolStripMenuItem.Size = new System.Drawing.Size(71, 20);
+      this.очиститьToolStripMenuItem.Text = "Очистить";
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(800, 450);
+      this.Controls.Add(this.chartOfIntegrals);
+      this.Controls.Add(this.label4);
+      this.Controls.Add(this.textBox4);
+      this.Controls.Add(this.label3);
+      this.Controls.Add(this.textBox3);
+      this.Controls.Add(this.label2);
+      this.Controls.Add(this.textBox2);
+      this.Controls.Add(this.label1);
+      this.Controls.Add(this.textBox1);
+      this.Controls.Add(this.menuStrip1);
+      this.MainMenuStrip = this.menuStrip1;
+      this.Name = "Form1";
+      this.Text = "Form1";
+      ((System.ComponentModel.ISupportInitialize)(this.chartOfIntegrals)).EndInit();
+      this.menuStrip1.ResumeLayout(false);
+      this.menuStrip1.PerformLayout();
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.TextBox textBox1;
+    private System.Windows.Forms.Label label1;
+    private System.Windows.Forms.Label label2;
+    private System.Windows.Forms.TextBox textBox2;
+    private System.Windows.Forms.Label label3;
+    private System.Windows.Forms.TextBox textBox3;
+    private System.Windows.Forms.Label label4;
+    private System.Windows.Forms.TextBox textBox4;
+    private System.Windows.Forms.DataVisualization.Charting.Chart chartOfIntegrals;
+    private System.Windows.Forms.MenuStrip menuStrip1;
+    private System.Windows.Forms.ToolStripMenuItem рассчитатьToolStripMenuItem;
+    private System.Windows.Forms.ToolStripMenuItem очиститьToolStripMenuItem;
+  }
+}
+
---
+++ Lab6/Интегралы/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных со сборкой.
+[assembly: AssemblyTitle("Интегралы")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Интегралы")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, следует установить атрибут ComVisible в TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("135700a4-76a6-4a0f-9e1f-ce6e791322e8")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
